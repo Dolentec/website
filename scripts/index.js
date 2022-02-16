3dbd5baae083cc800e521b723fa7bfb862558e8d
Dolentec <dolentec3474@gmail.com>
2022-02-16 17:41:16 +0100

    Hi

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+/*
+Function
+*/
+
+// var
+var birthYear;
+birthYear = Number(document.getElementById("input001").value)
+
+// function
+function calculateAge(birthYear) {
+    "use strict";
+    console.log(2022 - birthYear); // make it a number
+}
